b261239169b7ebd780fd1904d5de204b18a17157
Hildeberto Mendonca <me@hildeberto.com>
2015-10-30 23:30:55 +0100

    Simplified model and added new entities to the models.

--- studies/models.py
+++ studies/models.py
@@ -1,138 +1,111 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Offer(models.Model):
-    num_offer   = models.BigIntegerField()
-    acronym     = models.CharField(max_length = 5,blank = False, null = False)
-    cycle       = models.DecimalField(max_digits = 1, decimal_places = 0, blank = True, null = True)
-    level       = models.DecimalField(max_digits = 1, decimal_places = 0, blank = True, null = True)
-    offer_type  = models.CharField(max_length = 2, blank = True, null = True)
-    orientation = models.CharField(max_length = 2, blank = True, null = True)
+
+class AcademicYear(models.Model):
+    year = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
 
     def __str__(self):
-        return self.acronym.upper() + " " + str(self.cycle) + " " + str(self.level) + " " + self.offer_type + " " + self.orientation
+        return str(self.year)
 
+
+class Offer(models.Model):
+    title   = models.CharField(max_length = 240, blank = False, null = False)
+    acronym = models.CharField(max_length = 10, blank = True, null = True)
+
+    def __str__(self):
+        return self.title
 
 class OfferYear(models.Model):
-    title = models.CharField(max_length = 240, blank = True, null = True)
     offer = models.ForeignKey(Offer)
+    academic_year = models.ForeignKey(AcademicYear)
+
+    def __str__(self):
+        return self.offer.title + "(" + self.academic_year.year + ")"
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length = 50, blank = False, null = False)
+    last_name  = models.CharField(max_length = 50, blank = False, null = False)
+
+    def __str__(self):
+        return first_name + " " + last_name;
+
+
+class OfferEnrollment(models.Model):
+    offer_year         = models.ForeignKey(OfferYear)
+    student            = models.ForeignKey(Student)
+
+    def __str__(self):
+        return self.offer_year
+
+
+class Tutor(models.Model):
+    first_name = models.CharField(max_length = 50, blank = False, null = False)
+    last_name  = models.CharField(max_length = 50, blank = False, null = False)
+
+    def __str__(self):
+        return first_name + " " + last_name;
+
+
+class Structure(models.Model):
+    name = models.CharField(max_length = 100, blank = False, null = False)
+
+
+class LearningUnit(models.Model):
+    title = models.CharField(max_length = 210, blank = True, null = True)
+    acronym = models.CharField(max_length = 10, blank = True, null = True)
 
     def __str__(self):
         return self.title
 
 
-class Student(models.Model):
-    name                = models.CharField(max_length = 40, blank = False, null = False)
-    first_name1         = models.CharField(max_length = 20, blank = False, null = False)
-    registration_number = models.CharField(max_length = 10, blank = True, null = True)
-
-    def offer_enrollments(self):
-        return OfferEnrollment.objects.filter(student=self)
-    def learning_unit_enrollments(self):
-        return LearningUnitEnrollment.objects.filter(student=self)
-    def __str__(self):
-        return self.name + ' ' + self.first_name1
-
-
-class AcademicYear(models.Model):
-    year = models.IntegerField(validators=[
-                                        MaxValueValidator(3000),
-                                        MinValueValidator(1967)
-                                    ]
-                                 )
-    def __str__(self):
-        return str(self.year)
-
-
-class OfferEnrollment(models.Model):
-    REGISTRATION_STATE_CHOICES = (
-        ('UNKNOWN','Unknown'),
-        ('ERROR','Error'),
-        ('CYCLE','Cycle')
-    )
-
-    offer_year         = models.ForeignKey(OfferYear)
-    student            = models.ForeignKey(Student)
-    academic_year      = models.ForeignKey(AcademicYear, null = True)
-    registration_state = models.CharField(max_length = 10, blank = True, null = True,choices = REGISTRATION_STATE_CHOICES, default = 'UNKNOWN')
-
-    def __str__(self):
-        return self.registration_state
-
-
-# class Tutor(models.Model):
-# class Exam(models.Model):
-# class ExamEnrollment(models.Model):
-
-class LearningUnit(models.Model):
-    acronym     = models.CharField(max_length = 5,blank = False, null = False)
-    number      = models.DecimalField(max_digits = 11, decimal_places = 0, blank = True, null = True)
-    title       = models.CharField(max_length = 210, blank = True, null = True)
-
-    def __str__(self):
-        return self.acronym.upper() + " " + str(self.number)
-
-
 class LearningUnitYear(models.Model):
-    EXAM_REGISTRATION_STATE_CHOICES = (
-        ('REGISTERED', 'Registered'),
-        ('MODIFIED_TEST', 'Modified test'),
-        ('DELAY','Delay'),
-        ('SECOND_REGISTRATION','Second registration'),
-        ('SATISFYING','Satisfying'),
-        ('NOT_SATISFYING','Not Satisfying'),
-        ('CREDITS_DELAY','Credits delay'),
-        ('EXEMPTION', 'Exemption'),
-        ('JANUARY_SCORE_DELAY','January score delay'),
-        ('Q_94', 'Q_94'),
-        ('C_94', 'C_94'),
-        ('N_94', 'N_94'),
-        ('K_94', 'K_94'),
-        ('TEST', 'TEST'),
-        ('EXTERNAL', 'External'),
-        ('INCOMPLETE','Incomplete'),
-        ('*','Star'),
-        ('ALL','All'),
-        ('UNKNOWN','Unknown')
-    )
-    CREDIT_TYPE_CHOICES = (
-        ('CREDIT','Credit'),
-        ('DELAY','Delay'),
-        ('EXEMPTION','Exemption'),
-        ('Q_94','Credit before Bologne'),
-        ('C_94','Delay before Bologne'),
-        ('MODIFIED_TEST','Modified test'),
-        ('POSTPONED','Postponed'),
-        ('UNKNOWN','Unknown')
-    )
-
-    academicYear   = models.ForeignKey(AcademicYear)
-    learning_unit  = models.ForeignKey(LearningUnit)
-    score_1        = models.DecimalField(max_digits = 4, decimal_places = 2, blank = True, null = True)
-    exam_state_1   = models.CharField(max_length = 10, blank = True, null = True,choices = EXAM_REGISTRATION_STATE_CHOICES, default = 'UNKNOWN')
-    score_2        = models.DecimalField(max_digits = 4, decimal_places = 2, blank = True, null = True)
-    exam_state_2   = models.CharField(max_length = 10, blank = True, null = True,choices = EXAM_REGISTRATION_STATE_CHOICES, default = 'UNKNOWN')
-    score_3        = models.DecimalField(max_digits = 4, decimal_places = 2, blank = True, null = True)
-    exam_state_3   = models.CharField(max_length = 10, blank = True, null = True,choices = EXAM_REGISTRATION_STATE_CHOICES, default = 'UNKNOWN')
-    weight         = models.DecimalField(max_digits = 4, decimal_places = 2, blank = True, null = True)
-    credit_type    = models.CharField(max_length = 10, blank = True, null = True,choices = CREDIT_TYPE_CHOICES, default = 'UNKNOWN')
+    academic_year = models.ForeignKey(AcademicYear)
+    learning_unit = models.ForeignKey(LearningUnit)
 
     def __str__(self):
-        return str(self.academicYear.year) + " " + self.learning_unit.acronym   + str(self.learning_unit.number)
+        return self.academicYear + " " + self.learning_unit.acronym
+
 
 class LearningUnitEnrollment(models.Model):
-    student            = models.ForeignKey(Student)
+    student = models.ForeignKey(Student)
     learning_unit_year = models.ForeignKey(LearningUnitYear)
 
     def __str__(self):
-        return self.student.name + " " + str(self.learning_unit_year.academicYear)
-# class Structure(models.Model):
-# class Attribution(models.Model):
+        return self.student
 
+
+class Exam(models.Model):
+    learning_unit_year = models.ForeignKey(LearningUnitYear)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return str(self.start_date) + " - " + str(self.end_date)
+
+
+class ExamEnrollment(models.Model):
+    exam = models.ForeignKey(Exam)
+    learning_unit_enrollment = models.ForeignKey(LearningUnitEnrollment)
+
+
+class ExamEnrollmentHistory(models.Model):
+    exam_enrollment = models.ForeignKey(ExamEnrollment)
+    change_date = models.DateTimeField()
+
+
+class Attribution(models.Model):
+    tutor = models.ForeignKey(Tutor)
+    learning_unit = models.ForeignKey(LearningUnit)
+    start_date = models.DateField()
+    end_date = models.DateField()
 
 class Configuration(models.Model):
     key  = models.CharField(max_length = 50, blank = False, null = False)
-    value = models.CharField(max_length = 255, blank = False, null = False)
+    value = models.CharField(max_length = 255, blank = True, null = True)
 
     def __str__(self):
         return self.key
