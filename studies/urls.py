0be00582feac7230037d25fdc82db1f9095dbd6c
Hildeberto Mendonca <me@hildeberto.com>
2015-10-31 23:51:26 +0100

    Use of html template to render content.

--- studies/urls.py
+++ studies/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls.i18n import i18n_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.studies, name='studies'),]
+    url(r'^$', views.studies, name='studies'),
+    url(r'^courses/', views.courses, name='courses'),
+    url(r'^exams/', views.exams, name='exams')]
--- studies/views.py
+++ studies/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+def courses(request):
+    return render(request, "courses.html", {})
+
+
+def exams(request):
+    return render(request, "exams.html", {})
+
+
 def studies(request):
     return render(request, "index.html", {})
