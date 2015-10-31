dc56aaba5c4b7e3d1058272dcbda633c0c1c5825
Hildeberto Mendonca <me@hildeberto.com>
2015-10-31 09:41:12 +0100

    Using template to render html content. That's preferable to printing manually HTML content.

--- studies/urls.py
+++ studies/urls.py
@@ -4,7 +4,4 @@ from django.conf.urls.i18n import i18n_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^student/(?P<id_student>\w+)/(?P<id_offer_year>\w+)/(?P<year>\w+)', views.student, name='student'),
-    url(r'^attestations/', views.attestations, name='attestations'),
-]
+    url(r'^$', views.studies, name='studies'),]
