6d055859dc0fe9219beb2fef67b0802ff9002a92
Leila Verpoorten <leila.verpoorten@uclouvain.be>
2016-03-22 14:53:01 +0100

    Tuning

--- admission/models/person.py
+++ admission/models/person.py
@@ -31,6 +31,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import ugettext_lazy as _
 
+
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('user', 'birth_date', 'gender')
     fieldsets = ((None, {'fields': ('user', 'birth_date', 'gender')}),)
@@ -73,6 +74,7 @@ class Person(models.Model):
     def __str__(self):
         return u"%s" % self.user
 
+
 def find_by_user(user):
 
     try:
@@ -113,11 +115,3 @@ def find_by_id(id):
         return Person.objects.get(pk=id)
     except:
         return None
-
-
-def profile_completed(user):
-    person_result = Person.objects.filter(user__id=user.id).first()
-    if person_result:
-         if person_result.gender:
-            return True
-    return False
