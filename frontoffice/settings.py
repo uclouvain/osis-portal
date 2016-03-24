85b0d9875dfd6ca7598c849110321515f4c3665d
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2016-03-24 15:49:46 +0100

    #15 Modification fichiers config pour prendre en compte fichiers config serveurs

--- frontoffice/settings.py
+++ frontoffice/settings.py
@@ -157,3 +157,8 @@ SERVER_EMAIL = DEFAULT_FROM_EMAIL
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
 LOGIN_REDIRECT_URL = '/admission/'
+
+try  :
+    from frontoffice.server_settings import *
+except ImportError:
+    pass
