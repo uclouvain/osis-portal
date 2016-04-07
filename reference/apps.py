eac29679cc32909d3a96e5eb4e8399c04e51f6a9
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2016-04-07 17:42:52 +0200

    #94 implementation of insert/update in osis-portal + launch of listen_queue

--- reference/apps.py
+++ reference/apps.py
@@ -24,7 +24,32 @@
 #
 ##############################################################################
 from django.apps import AppConfig
+from frontoffice import queue
+import json
+
 
 
 class ReferenceConfig(AppConfig):
     name = 'reference'
+
+    def insert_or_update(self, json_datas):
+        """
+        Insert the records in PostGreSQL. If the records already exist, then the method makes an update.
+        """
+        data = json.loads(json_datas.decode("utf-8"))
+        from reference import models
+        map_classes = {
+            'reference.Country': models.Country,
+        }
+        cls_str = data['model_class_str']
+        model_class = map_classes[cls_str]
+        records = data['records']
+        # ids = [obj['id'] for obj in records]
+        for record in records :
+            obj, created = model_class.objects.update_or_create(defaults=record, **{'id' : record['id']})
+            print("created ? : " + str(created))
+
+    def ready(self):
+        # if django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.
+        # ===> This exception says that there is an error in the implementation of method ready(self) !!
+        queue.listen_queue(self.name, self.insert_or_update)
