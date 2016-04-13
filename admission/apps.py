875f6c0db97d8a450742304c255663a035df441e
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2016-04-13 11:48:57 +0200

    #101 added creation of queue for migration of Admission.Domain

--- admission/apps.py
+++ admission/apps.py
@@ -1,4 +1,39 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2016 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
 from django.apps import AppConfig
+from frontoffice.queue import callbacks, queue
+import json
+
 
 class AdmissionConfig(AppConfig):
     name = 'admission'
+
+
+    def ready(self):
+        # if django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.
+        # ===> This exception says that there is an error in the implementation of method ready(self) !!
+        queue.listen_queue(self.name, callbacks.insert_or_update)
