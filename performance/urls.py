7c89b9c0e5bb1c55e7737ea97af02effaa29db11
edd19 <eddyndizera@gmail.com>
2016-07-07 16:14:31 +0200

    created performance app

--- performance/urls.py
+++ performance/urls.py
@@ -23,18 +23,9 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from django.contrib import admin
-from django.conf.urls import url, include
+from django.conf.urls import url
+from performance.views import main
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^admission/', include('admission.urls')),
-    url(r'^dashboard/', include('dashboard.urls')),
-    url(r'^catalog/', include('catalog.urls')),
-    url(r'^performance/', include('performance.urls'))
+    url(r'^$', main.home),
 ]
-
-try:
-    from frontoffice.server_urls import *
-except ImportError:
-    pass
