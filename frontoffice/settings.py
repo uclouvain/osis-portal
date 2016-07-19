c5ebc88ffd13409d51a3600ae1e469d584a26760
Christophe Fivet <christophe.fivet@uclouvain.be>
2016-07-19 15:03:10 +0200

    #51 Modification du lieu de stockage via une variable globale

--- frontoffice/settings.py
+++ frontoffice/settings.py
@@ -159,6 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
