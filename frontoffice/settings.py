095f814e0869e3235f5484f2356e03d71a681111
Christophe Fivet <christophe.fivet@uclouvain.be>
2016-07-20 11:29:51 +0200

    #51 Ajout de l'erreur dans le template d'exemple

--- frontoffice/settings.py
+++ frontoffice/settings.py
@@ -159,7 +159,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
 UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
+CONTENT_TYPES = ['application/csv', 'application/doc', 'application/pdf', 'application/xls', 'application/xml',
+                 'application/zip', 'image/jpeg', 'image/gif', 'image/png', 'text/html', 'text/plain']
+MAX_UPLOAD_SIZE = 5242880
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
