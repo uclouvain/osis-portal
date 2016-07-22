c1437d45ca92a033eb1bf000c9261b4907daa501
edd19 <eddyndizera@gmail.com>
2016-07-22 12:58:26 +0200

    tutor see only scores encoding and student only performance on dashboard

--- base/admin.py
+++ base/admin.py
@@ -31,3 +31,5 @@ admin.site.register(person.Person,
                     person.BasePersonAdmin)
 admin.site.register(student.Student,
                     student.StudentAdmin)
+admin.site.register(tutor.Tutor,
+                    tutor.TutorAdmin)
