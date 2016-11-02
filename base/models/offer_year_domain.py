c4e1c2f08db671875506fe3741695255a97537c2
Leila Verpoorten <leila.verpoorten@uclouvain.be>
2016-11-02 14:44:54 +0100

    #452 Corrections demandées dans le PR

--- base/models/offer_year_domain.py
+++ base/models/offer_year_domain.py
@@ -56,3 +56,7 @@ def search(level=None, domain=None):
                                               domain__in=domains)
     else:
         return []
+
+
+def find_by_offer_year(offer_yr):
+    return OfferYearDomain.objects.get(offer_year=offer_yr)
