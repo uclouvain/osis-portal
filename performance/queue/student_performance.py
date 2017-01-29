5a42225f66d13e9c3feb1dc3f554105c8345ad93
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2017-01-29 21:59:41 +0100

    #702 oubli add

--- performance/queue/student_performance.py
+++ performance/queue/student_performance.py
@@ -153,6 +153,6 @@ def update_expiration_date(registration_id, academic_year, acronym, new_exp_date
         performances_to_update = get_performances_by_offer(acronym=acronym, academic_year=academic_year)
 
     for performance in performances_to_update:
-        if performance.update_date:
+        if new_exp_date and new_exp_date != 'null':
             performance.update_date = datetime.datetime.fromtimestamp(new_exp_date / 1e3)
             performance.save()
