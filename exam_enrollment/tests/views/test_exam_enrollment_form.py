78ea2168453f94cb8d8aafcbf642eda331698624
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2017-02-23 18:25:43 +0100

    #696 unit tests + renaming of functions and code cleanup

--- exam_enrollment/tests/views/test_exam_enrollment_form.py
+++ exam_enrollment/tests/views/test_exam_enrollment_form.py
@@ -24,11 +24,13 @@
 #
 ##############################################################################
 from unittest.mock import patch
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group, Permission
+from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 import json
 import random
-from base.tests.models import test_student, test_person, test_academic_year
+from base.tests.models import test_student, test_person, test_academic_year, test_offer_year
+from exam_enrollment.views import main
 
 
 def load_json_file(path):
@@ -39,7 +41,8 @@ def load_json_file(path):
 
 def _create_group(name):
     group = Group(name=name)
-    return group.save()
+    group.save()
+    return group
 
 
 class ExamEnrollmentFormTest(TestCase):
@@ -47,11 +50,19 @@ class ExamEnrollmentFormTest(TestCase):
         self.client = Client()
         self.academic_year = test_academic_year.create_academic_year()
         group = _create_group('students')
-        # self.user.groups.add(group)
+        group.permissions.add(Permission.objects.get(codename='is_student'))
         self.user = User.objects.create_user(username='jsmith', email='jsmith@localhost', password='secret')
+        self.user_not_student = User.objects.create_user(username='pjashar', email='pjashar@localhost', password='secret')
+        self.user.groups.add(group)
         self.person = test_person.create_person_with_user(self.user, first_name="James", last_name="Smith")
-        self.student = test_student.create_student_with_registration_person("12345678", self.person)
-        self.url = '1234/form/'
+        self.registration_id = "12345678"
+        self.student = test_student.create_student_with_registration_person(self.registration_id, self.person)
+        offer_year_id = 1234
+        self.off_year = test_offer_year.create_offer_year_from_kwargs(**{'id': offer_year_id,
+                                                                         'acronym': 'SINF1BA',
+                                                                         'title': 'Bechelor in informatica',
+                                                                         'academic_year': self.academic_year})
+        self.url = "/exam_enrollment/{}/form/".format(offer_year_id)
         self.correct_exam_enrol_form = load_json_file("exam_enrollment/tests/ressources/exam_enrollment_form_example.json")
 
     def test_json_form_content(self):
@@ -70,29 +81,69 @@ class ExamEnrollmentFormTest(TestCase):
                         or 'session_3' in random_exam_enrol.keys())
         self.assertTrue(random_exam_enrol.get('learning_unit_year'))
 
-    @patch("osis_common.queue.queue_sender.send_message")
-    def test_exam_enrollment_form(self, mock_send_message):
-        mock_send_message.return_value = self.correct_exam_enrol_form
-        # self.client.login(username='jsmith', password='secret')
+    @patch("exam_enrollment.views.main._fetch_exam_enrollment_form")
+    def test_exam_enrollment_form(self, fetch_json):
+        fetch_json.return_value = self.correct_exam_enrol_form
+        self.client.force_login(self.user)
         response = self.client.get(self.url)
-        print(str(response))
-        print("------------------")
-        print(str(response.context))
-        print("------------------")
-        print(str(response.content))
-        print("------------------")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('exam_enrollments' in response.context)
-        self.assertTrue('student' in response.context)
-        self.assertTrue('current_number_session' in response.context)
-        self.assertTrue('academic_year' in response.context)
-        self.assertTrue('program' in response.context)
+        self.assertTemplateUsed(response, 'exam_enrollment_form.html')
+        returned_data = response.context[-1]
+        self.assertIn('exam_enrollments', returned_data)
+        self.assertIn('student', returned_data)
+        self.assertIn('current_number_session', returned_data)
+        self.assertIn('academic_year', returned_data)
+        self.assertIn('program', returned_data)
 
-    def test_return_content(self):
-        pass
+    @patch("exam_enrollment.views.main._fetch_exam_enrollment_form")
+    def test_outside_score_encoding_period(self, fetch_json):
+        fetch_json.return_value = None
+        self.client.force_login(self.user)
+        response = self.client.get(self.url, follow=True)
+        self.assertRedirects(response, reverse('dashboard_home'))
 
+    def test_case_user_is_not_student(self):
+        self.client.force_login(self.user_not_student)
+        response = self.client.get(self.url, follow=True)
+        self.assertTemplateUsed(response, 'access_denied.html')
 
-
-
-
-
+    @patch("osis_common.queue.queue_sender.send_message")
+    def test_exam_enrollment_form_submission_message(self, send_message):
+        send_message.return_value = None
+        self.client.force_login(self.user)
+        post_data = {
+            "chckbox_exam_enrol_sess1_LPHYS1234": "on",
+            "etat_to_inscr_current_session_LPHYS1234": "I",
+            "chckbox_exam_enrol_sess1_LBIO4567": "",
+            "etat_to_inscr_current_session_LBIO4567": "Y",
+            "chckbox_exam_enrol_sess1_LDROI1111": None,
+            "etat_to_inscr_current_session_LDROI1111": "P",
+            "current_number_session": 1,
+        }
+        # from django.contrib.messages.storage import default_storage
+        # request._messages = default_storage(request)
+        exam_enrollments_expected = [{"acronym": "LPHYS1234",
+                                      "is_enrolled": True,
+                                      "etat_to_inscr": "I"},
+                                     {"acronym": "LBIO4567",
+                                      "is_enrolled": False,
+                                      "etat_to_inscr": "Y"},
+                                     {"acronym": "LDROI1111",
+                                      "is_enrolled": False,
+                                      "etat_to_inscr": "P"}]
+        expected_result = {
+            "registration_id": self.registration_id,
+            "offer_year_acronym": self.off_year.acronym,
+            "year": self.off_year.academic_year.year,
+            "exam_enrollments": exam_enrollments_expected
+        }
+        response = self.client.post(self.url, post_data)
+        result = main._exam_enrollment_form_submission_message(self.off_year, response.wsgi_request, self.student)
+        self.assertEqual(len(result), len(expected_result))
+        self.assertEqual(expected_result.get('registration_id'), result.get('registration_id'))
+        self.assertEqual(expected_result.get('offer_year_acronym'), result.get('offer_year_acronym'))
+        self.assertEqual(expected_result.get('year'), result.get('year'))
+        exam_enrollments = result.get('exam_enrollments')
+        exam_enrollments_expected = expected_result.get('exam_enrollments')
+        for index in range(0, len(exam_enrollments)):
+            self.assertIn(exam_enrollments[index], exam_enrollments_expected)
--- exam_enrollment/views/main.py
+++ exam_enrollment/views/main.py
@@ -32,8 +32,6 @@ from frontoffice.queue import queue_listener
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from django.http import response
-from exam_enrollment.forms.exam_enrollment_form import ExamEnrollmentForm
-from django.forms import formset_factory
 from osis_common.queue import queue_sender
 from django.conf import settings
 
@@ -54,101 +52,84 @@ def choose_offer(request):
 def exam_enrollment_form(request, offer_year_id):
     stud = student.find_by_user(request.user)
     off_year = offer_year.find_by_id(offer_year_id)
-    # ExamEnrollmentFormSet = formset_factory(ExamEnrollmentForm, extra=0)
     if request.method == 'POST':
-        # formset = ExamEnrollmentFormSet(request.POST, request.FILES)
-        # if formset.is_valid():
-        #     for f in formset:
-        #         print(str(f.cleaned_data))
-        #         print('ici')
-        #     pass
-
-        data_to_submit = {
-            'registration_id': stud.registration_id,
-            'offer_year_acronym': off_year.acronym,
-            'year': off_year.academic_year.year,
-            'exam_enrollments': _build_enrollments_by_learning_unit(request)
-        }
-        queue_sender.send_message(settings.QUEUES.get('QUEUES_NAME').get('EXAM_ENROLLMENT_FORM_SUBMISSION'),
-                                  data_to_submit)
-        messages.add_message(request, messages.SUCCESS, _('exam_enrollment_form_submitted'))
-        return response.HttpResponseRedirect(reverse('dashboard_home'))
+        return _process_exam_enrollment_form_submission(off_year, request, stud)
     else:
-        data = None
-        if stud:
-            data = _fetch_json(stud.registration_id, off_year.acronym, off_year.academic_year.year)
-            if not data:
-                messages.add_message(request, messages.WARNING, _('outside_exam_enrollment_period'))
-                return response.HttpResponseRedirect(reverse('dashboard_home'))
-            # test = ExamEnrollmentForm()
-            # formset = ExamEnrollmentFormSet(initial=data.get('exam_enrollments'))
-            # test = ExamEnrollmentFormSet(**data)
-            return layout.render(request, 'exam_enrollment_form.html', {'exam_enrollments': data.get('exam_enrollments'),
-                                                                        'student': stud,
-                                                                        'current_number_session': data.get('current_number_session'),
-                                                                        'academic_year': academic_year.current_academic_year(),
-                                                                        'program': offer_year.find_by_id(offer_year_id)})
+        return _get_exam_enrollment_form(off_year, offer_year_id, request, stud)
+
+
+def _get_exam_enrollment_form(off_year, offer_year_id, request, stud):
+    data = _fetch_exam_enrollment_form(stud.registration_id, off_year.acronym, off_year.academic_year.year)
+    if not data:
+        messages.add_message(request, messages.WARNING, _('outside_exam_enrollment_period'))
+        return response.HttpResponseRedirect(reverse('dashboard_home'))
+    return layout.render(request, 'exam_enrollment_form.html', {'exam_enrollments': data.get('exam_enrollments'),
+                                                                'student': stud,
+                                                                'current_number_session': data.get('current_number_session'),
+                                                                'academic_year': academic_year.current_academic_year(),
+                                                                'program': offer_year.find_by_id(offer_year_id)})
+
+
+def _process_exam_enrollment_form_submission(off_year, request, stud):
+    data_to_submit = _exam_enrollment_form_submission_message(off_year, request, stud)
+    queue_sender.send_message(settings.QUEUES.get('QUEUES_NAME').get('EXAM_ENROLLMENT_FORM_SUBMISSION'),
+                              json.dumps(data_to_submit))
+    messages.add_message(request, messages.SUCCESS, _('exam_enrollment_form_submitted'))
+    return response.HttpResponseRedirect(reverse('dashboard_home'))
+
+
+def _exam_enrollment_form_submission_message(off_year, request, stud):
+    return {
+        'registration_id': stud.registration_id,
+        'offer_year_acronym': off_year.acronym,
+        'year': off_year.academic_year.year,
+        'exam_enrollments': _build_enrollments_by_learning_unit(request)
+    }
 
 
 def _build_enrollments_by_learning_unit(request):
     current_number_session = request.POST['current_number_session']
-    # "etat_to_inscr_current_session"
     enrollments_by_learn_unit = []
-    is_enrolled_by_acronym = [{"acronym": _extract_acronym(html_tag_id), "is_enrolled": True if value == "on" else False} for html_tag_id, value in request.POST.items()
-                              if "chckbox_exam_enrol_sess{}_".format(current_number_session) in html_tag_id]
-    etat_to_inscr_by_acronym = {_extract_acronym(html_tag_id): etat_to_inscr for html_tag_id, etat_to_inscr in request.POST.items()
-                                if "etat_to_inscr_current_session_" in html_tag_id}
-    # append_etat_to_inscr_value = lambda x: x.update({"etat_to_inscr": etat_to_inscr_by_acronym[x.get('acronym')]})
-    for is_enrol_by_acronym in is_enrolled_by_acronym:
-        acronym = is_enrol_by_acronym.get('acronym')
-        is_enrol_by_acronym['etat_to_inscr'] = etat_to_inscr_by_acronym[acronym]
-        enrollments_by_learn_unit.append(is_enrol_by_acronym)
+    is_enrolled_acronym_dicts = _build_dicts_is_enrolled_by_acronym(current_number_session, request)
+    etat_to_inscr_by_acronym = _build_dicts_etat_to_inscr_by_acronym(request)
+    for is_enrol_acronym_dict in is_enrolled_acronym_dicts:
+        acronym = is_enrol_acronym_dict.get('acronym')
+        enrollments_by_learn_unit.append({
+            "acronym": acronym,
+            "is_enrolled": is_enrol_acronym_dict.get('is_enrolled'),
+            "etat_to_inscr": etat_to_inscr_by_acronym[acronym]
+        })
     return enrollments_by_learn_unit
-    # liste = [_append_etat_to_inscr_item(is_enrol_by_acronym, etat_to_inscr_by_acronym) for is_enrol_by_acronym in is_enrolled_by_acronym]
-    # return liste
-
-    # for key, value in request.POST.items():
-    #     enrol_by_learn_unit = {}
-    #     if "chckbox_exam_enrol_sess{}_".format(current_number_session) in key:
-    #         enrol_by_learn_unit["acronym"] = key.split("_")[-1]
-    #         enrol_by_learn_unit["is_enrolled"] = value
-    #
-    # return [{"acronym": key.split("_")[-1], "is_enrolled": value} for key, value in request.POST.items()
-    #         if "chckbox_exam_enrol_sess{}_".format(current_number_session) in key]
 
 
-def _append_etat_to_inscr_item(a_dict, etat_to_inscr_by_acronym):
-    a_dict['etat_to_inscr'] = etat_to_inscr_by_acronym(a_dict.get('acronym'))
-    return a_dict
+def _build_dicts_etat_to_inscr_by_acronym(request):
+    return {_extract_acronym(html_tag_id): etat_to_inscr for html_tag_id, etat_to_inscr in request.POST.items()
+            if "etat_to_inscr_current_session_" in html_tag_id}
+
+
+def _build_dicts_is_enrolled_by_acronym(current_number_session, request):
+    return [{"acronym": _extract_acronym(html_tag_id), "is_enrolled": True if value == "on" else False}
+            for html_tag_id, value in request.POST.items()
+            if "chckbox_exam_enrol_sess{}_".format(current_number_session) in html_tag_id]
+
 
 def _extract_acronym(html_tag_id):
     return html_tag_id.split("_")[-1]
 
 
-def _extract(big_dict, keys_to_extract):
-    return {k: big_dict[k] for k in big_dict.keys() & keys_to_extract}
-
-
-def _fetch_json(registration_id, offer_year_acronym, year):
-    # return load_json_file("exam_enrollment/tests/ressources/exam_enrollment_form_example.json")
+def _fetch_exam_enrollment_form(registration_id, offer_year_acronym, year):
     exam_enrol_client = queue_listener.ExamEnrollmentClient()
-    message = _generate_message(registration_id, offer_year_acronym, year)
-    json_data = exam_enrol_client.call(message)
+    message = _exam_enrollment_form_message(registration_id, offer_year_acronym, year)
+    json_data = exam_enrol_client.call(json.dumps(message))
     if json_data:
         json_data = json_data.decode("utf-8")
         return json.loads(json_data)
     return json_data
 
-def load_json_file(path):
-    json_data = open(path)
-    data1 = json.load(json_data) # deserialises it
-    return data1
 
-
-def _generate_message(registration_id, offer_year_acronym, year):
-    message = {
+def _exam_enrollment_form_message(registration_id, offer_year_acronym, year):
+    return {
         'registration_id': registration_id,
         'offer_year_acronym': offer_year_acronym,
         'year': year,
     }
-    return json.dumps(message)
