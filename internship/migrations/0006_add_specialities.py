061f9f9d7b829887da20ac5a1138aac3ad3bd9ba
Ndizera Eddy <eddyndizera@gmail.com>
2017-02-27 18:40:45 +0100

    create offer for personal internship

--- internship/migrations/0006_add_specialities.py
+++ internship/migrations/0006_add_specialities.py
@@ -13,12 +13,22 @@ def forwards_func(apps, schema_editor):
         LearningUnit.objects.using(db_alias).get_or_create(acronym="WMDS2333", defaults={'title': 'Stages'})
 
     Speciality = apps.get_model("internship", "InternshipSpeciality")
-    Speciality.objects.using(db_alias).get_or_create(acronym="STAGE PERSONNEL",
+    personal_speciality, _ = Speciality.objects.using(db_alias).get_or_create(acronym="STAGE PERSONNEL",
                                                      defaults={"name": "Stage personnel",
                                                                "learning_unit": learning_unit_internship})
     Speciality.objects.using(db_alias).get_or_create(acronym="STAGE A L'ETRANGER",
                                                      defaults={"name": "Stage à l'étranger",
                                                                "learning_unit": learning_unit_internship})
+
+    Organization = apps.get_model("internship", "Organization")
+    personal_organization, _ = Organization.objects.using(db_alias).get_or_create(acronym="STAGE PERSONNEL",
+                                                       defaults={"name": "Stage personnel"})
+
+    InternshipOffer = apps.get_model("internship", "InternshipOffer")
+    InternshipOffer.objects.using(db_alias).get_or_create(organization=personal_organization,
+                                                          speciality=personal_speciality,
+                                                          defaults={"title": "Stage personnel",
+                                                                    "maximum_enrollments": 999})
 
 
 class Migration(migrations.Migration):
