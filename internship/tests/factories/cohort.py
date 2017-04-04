3fa353b4e17d46dae8c4adf43758c996716962e1
Marc Lainez <marc.lainez@gmail.com>
2017-04-04 15:51:12 +0200

    Force factories to use default database

--- internship/tests/factories/cohort.py
+++ internship/tests/factories/cohort.py
@@ -18,6 +18,7 @@ def fn_subscription_end_date(cohort):
 class CohortFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'internship.Cohort'
+        database = 'default'
 
     name = factory.Sequence(lambda n: 'Cohort %d' % (n,))
     description = factory.fuzzy.FuzzyText()
--- internship/tests/factories/internship.py
+++ internship/tests/factories/internship.py
@@ -3,11 +3,11 @@ import factory.fuzzy
 import pendulum
 
 from internship.tests.factories.cohort import CohortFactory
-from internship.tests.models.test_internship_speciality import create_speciality
 
 class InternshipFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'internship.Internship'
+        database = 'default'
 
     name = factory.Sequence(lambda n: 'Cohort %d' % (n,))
     length_in_periods = 1
