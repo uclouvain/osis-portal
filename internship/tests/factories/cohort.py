b906e093f58bedd15debffe51905e95c05370ac3
Marc Lainez <marc.lainez@gmail.com>
2017-04-06 19:05:49 +0200

    Fix tests

--- internship/tests/factories/cohort.py
+++ internship/tests/factories/cohort.py
@@ -8,7 +8,7 @@ def fn_publication_start_date(cohort):
 
 
 def fn_subscription_start_date(cohort):
-    return pendulum.instance(cohort.publication_start_date).add(months=1)._datetime
+    return pendulum.instance(cohort.publication_start_date).subtract(months=1)._datetime
 
 
 def fn_subscription_end_date(cohort):
