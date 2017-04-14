175f81b3c8277a3fdb5261266acc87f4679ae3e0
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2017-04-14 13:31:57 +0200

    #1000 supression config inutile + gestion des authentifications

--- frontoffice/settings/base.py
+++ frontoffice/settings/base.py
@@ -26,7 +26,6 @@
 import os
 from django.core.urlresolvers import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
-import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -34,11 +33,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY Settings
 # Those settings are mandatory and have to be defined in your .env file
 SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = bool(os.environ.get('DEBUG', False))
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split()
 ADMIN_URL = os.environ['ADMIN_URL']
 ENVIRONMENT = os.environ['ENVIRONMENT']
-CSRF_COOKIE_SECURE = bool(os.environ.get('CSRF_COOKIE_SECURE', False))
+CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', 'False').lower() == 'true'
+
 
 # Base configuration
 ROOT_URLCONF = 'frontoffice.urls'
@@ -126,9 +126,9 @@ LANGUAGES = [
 ]
 # You can change default values for internalizations settings in your .env file
 TIME_ZONE = os.environ.get('TIME_ZONE', 'Europe/Brussels')
-USE_I18N = bool(os.environ.get('USE_I18N', True))
-USE_L10N = bool(os.environ.get('USE_L10N', True))
-USE_TZ = bool(os.environ.get('USE_TZ', True))
+USE_I18N = os.environ.get('USE_I18N', 'True').lower() == 'true'
+USE_L10N = os.environ.get('USE_L10N', 'True').lower() == 'true'
+USE_TZ = os.environ.get('USE_TZ', 'True').lower() == 'true'
 
 
 # Static files (CSS, JavaScript, Images) and Media
@@ -154,21 +154,21 @@ QUEUE_EXCEPTION_LOGGER = os.environ.get('QUEUE_EXCEPTION_LOGGER', 'queue_excepti
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'osis@localhost.be')
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', DEFAULT_FROM_EMAIL)
 LOGO_EMAIL_SIGNATURE_URL = os.environ.get('LOGO_EMAIL_SIGNATURE_URL', '')
-EMAIL_PRODUCTION_SENDING = bool(os.environ.get('EMAIL_PRODUCTION_SENDING', False))
+EMAIL_PRODUCTION_SENDING = os.environ.get('EMAIL_PRODUCTION_SENDING', 'False').lower() == 'true'
 COMMON_EMAIL_RECEIVER = os.environ.get('COMMON_EMAIL_RECEIVER', 'osis@localhost.org')
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.filebased.EmailBackend')
 EMAIL_FILE_PATH = os.environ.get('EMAIL_FILE_PATH', os.path.join(BASE_DIR, "base/tests/sent_mails"))
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 25))
-SEND_BROKEN_LINK_EMAILS = bool(os.environ.get('SEND_BROKEN_LINK_EMAILS', True))
+SEND_BROKEN_LINK_EMAILS = os.environ.get('SEND_BROKEN_LINK_EMAILS', 'True').lower() == 'true'
 
 
 # Authentication settings
 LOGIN_URL = os.environ.get('LOGIN_URL', reverse_lazy('login'))
-OVERRIDED_LOGOUT_URL = os.environ.get('OVERRIDED_LOGOUT_URL', '')
-OVERRIDED_LOGIN_URL = os.environ.get('OVERRIDED_LOGIN_URL', '')
-LOGOUT_BUTTON = bool(os.environ.get('LOGOUT_BUTTON', True))
-CURRENT_URL = os.environ.get('CURRENT_URL', 'http://localhost:8000')
+LOGIN_REDIRECT_URL = os.environ.get('LOGIN_REDIRECT_URL', reverse_lazy('dashboard_home'))
+OVERRIDED_LOGIN_URL = os.environ.get('OVERRIDED_LOGIN_URL', None)
+OVERRIDED_LOGOUT_URL = os.environ.get('OVERRIDED_LOGOUT_URL', None)
+LOGOUT_BUTTON = os.environ.get('LOGOUT_BUTTON', 'True').lower() == 'true'
 
 
 # This has to be set in your .env with the actual url where you institution logo can be found.
--- frontoffice/settings/local.py
+++ frontoffice/settings/local.py
@@ -24,6 +24,7 @@
 #
 ##############################################################################
 from .base import *
+import sys
 
 INSTALLED_APPS += (
     'dashboard',
