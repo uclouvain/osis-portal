fb0bba39cf25b73bfb6b7d0dcb238811bf9cfa79
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2017-09-19 11:51:40 +0200

    L'année académique actuelle et plus récent doit s'afficher par défaut

--- attribution/views/tutor_charge.py
+++ attribution/views/tutor_charge.py
@@ -76,7 +76,7 @@ def home(request):
 
 def get_current_academic_year():
     a_year = datetime.datetime.now().year
-    current_academic_year = mdl_base.academic_year.current_academic_year()
+    current_academic_year = mdl_base.academic_year.starting_academic_year()
     if current_academic_year:
         a_year = current_academic_year.year
     return a_year
