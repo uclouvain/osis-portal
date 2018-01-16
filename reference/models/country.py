abcd691276aaec23e05ea189db4060599ea0669d
Ndizera Eddy <eddyndizera@gmail.com>
2018-01-16 11:54:48 +0100

    Ensure pep8 for models/country.py

--- reference/models/country.py
+++ reference/models/country.py
@@ -29,7 +29,8 @@ from osis_common.models.serializable_model import SerializableModel, Serializabl
 
 class CountryAdmin(SerializableModelAdmin):
     list_display = ('external_id', 'name', 'iso_code', 'nationality', 'european_union', 'dialing_code', 'cref_code')
-    fieldsets = ((None, {'fields': ('iso_code', 'name', 'nationality', 'european_union', 'dialing_code', 'cref_code')}),)
+    fieldsets = ((None, {'fields': ('iso_code', 'name', 'nationality', 'european_union', 'dialing_code',
+                                    'cref_code')}),)
     ordering = ('name',)
     search_fields = ['name']
 
