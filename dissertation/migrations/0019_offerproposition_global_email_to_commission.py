ec30d7f407745a9df7eab78c55456f6d4e78d320
Laurent Dermine <Laurent.Dermine@uclouvain.be>
2018-03-15 13:25:25 +0100

    dissertation: change migration file to 0019

--- dissertation/migrations/0019_offerproposition_global_email_to_commission.py
+++ dissertation/migrations/0019_offerproposition_global_email_to_commission.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dissertation', '0017_auto_20171010_1535'),
+        ('dissertation', '0018_auto_20180306_1739'),
     ]
 
     operations = [
