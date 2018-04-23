bc849b0b6a653cc733f73d7a7b6247bbcb02d403
Hildeberto Mendonca <me@hildeberto.com>
2018-04-23 12:03:56 +0200

    Modifications in the model to make it similar to the model in osis

--- models/enums/affectation_type.py
+++ models/enums/affectation_type.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2016 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2017 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,23 +23,10 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from django.db import models
-from osis_common.models.serializable_model import SerializableModelAdmin, SerializableModel
+from osis_common.utils.enumerations import ChoiceEnum
 
 
-class CohortAdmin(SerializableModelAdmin):
-    list_display = ('name', 'description', 'publication_start_date',
-                    'subscription_start_date', 'subscription_end_date')
-    fieldsets = ((None, {'fields': ('name', 'description', 'publication_start_date',
-                                    'subscription_start_date', 'subscription_end_date')}),)
-
-
-class Cohort(SerializableModel):
-    name = models.CharField(max_length=255, blank=False)
-    description = models.TextField()
-    publication_start_date = models.DateField(blank=False)
-    subscription_start_date = models.DateField(blank=False)
-    subscription_end_date = models.DateField(blank=False)
-
-    def __str__(self):
-        return u"%s" % self.name
+class AffectationType(ChoiceEnum):
+    PRIORITY = "S"
+    NORMAL = "N"
+    ERROR = "X"
