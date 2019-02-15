dc1958e16b2fc1fed7391b58cbd8c78906b55ffa
BenJ <benjamin.daubry@uclouvain.be>
2019-02-15 15:02:32 +0100

    Add test to test country autocomplete

---
+++ base/tests/views/test_country.py
@@ -0,0 +1,58 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+import json
+from unittest import mock
+
+from django.test import TestCase
+from django.urls import reverse
+
+from base.tests.factories.user import SuperUserFactory
+from reference.tests.factories.country import CountryFactory
+
+
+class TestCountryAutocomplete(TestCase):
+    def setUp(self):
+        self.super_user = SuperUserFactory()
+        self.url = reverse("country-autocomplete")
+        self.country = CountryFactory(name="Narnia")
+
+    @mock.patch('base.views.country.CountryAutocomplete.get_country_list_from_osis', return_value=[{'name': 'Narnia'}])
+    def test_when_filter(self, mock_country):
+        self.client.force_login(user=self.super_user)
+        response = self.client.get(self.url, data={'q': 'nar'})
+
+        self.assertEqual(response.status_code, 200)
+
+        results = _get_results_from_autocomplete_response(response)
+
+        expected_results = [{'id': 'Narnia', 'text': 'Narnia'}]
+
+        self.assertListEqual(results, expected_results)
+
+
+def _get_results_from_autocomplete_response(response):
+    json_response = str(response.content, encoding='utf8')
+    return json.loads(json_response)['results']
