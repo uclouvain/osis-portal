b42f5a7a86bd45a03670d5f689c59acf5006cb9b
BenJ <benjamin.daubry@uclouvain.be>
2019-02-18 13:47:17 +0100

    Reviewed

--- base/views/country.py
+++ base/views/country.py
@@ -1,11 +1,9 @@
-import io
 import json
 
 import requests
 from dal import autocomplete
 from django import http
 from django.conf import settings
-from rest_framework.parsers import JSONParser
 
 
 class CountryAutocomplete(autocomplete.Select2ListView):
@@ -14,16 +12,9 @@ class CountryAutocomplete(autocomplete.Select2ListView):
         return http.HttpResponse(json.dumps({
             'results': [
                 {'id': country['uuid'], 'text': country['name']}
-                for country in self.get_countries_list(name_filter=self.q)
+                for country in self.get_country_list_from_osis(name_filter=self.q)
             ]
         }), content_type='application/json')
-
-    def get_countries_list(self, name_filter=None):
-        list_countries = []
-        list_country = self.get_country_list_from_osis(name_filter)
-        for country in list_country:
-            list_countries.append({'uuid': country['uuid'], 'name': country['name']})
-        return list_countries
 
     def get_country_list_from_osis(self, name_filter=None):
         header_to_get = {'Authorization': 'Token ' + settings.OSIS_PORTAL_TOKEN}
@@ -35,8 +26,7 @@ class CountryAutocomplete(autocomplete.Select2ListView):
             data={'search': name_filter or ""}
         )
 
-        stream = io.BytesIO(response.content)
-        data = JSONParser().parse(stream)
+        data = response.json()
         if 'results' in data:
             data = data['results']
         return data
