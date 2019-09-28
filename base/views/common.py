3d7ed03de88701971c6d396ec009333b1a850b70
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2019-09-28 17:26:42 +0200

    Supression de mise en session + accès menu

--- base/views/common.py
+++ base/views/common.py
@@ -64,7 +64,6 @@ def common_context_processor(request):
                'debug': settings.DEBUG,
                'logout_button': settings.LOGOUT_BUTTON}
     _check_notice(request, context)
-    _set_managed_programs(request, context)
     return context
 
 
@@ -78,46 +77,6 @@ def _check_notice(request, context):
     if 'subject' in request.session and 'notice' in request.session:
         context['subject'] = request.session['subject']
         context['notice'] = request.session['notice']
-
-
-def _set_managed_programs(request, context):
-    """
-    1. Preconditions : user is authenticated and user is not a student.
-    2. Check if the session key 'is_faculty_manager' is defined
-        2.1. If yes, Context 'is_faculty_manager' is updated with value of Session 'is_faculty_manager'
-        2.2. If not :
-            2.2.1: The managed programs are retrieved from osis with call to the api
-            2.2.2: If the managed programs exists :
-                2.2.2.1: Context 'is_faculty_manager' value is set to True
-                2.2.2.2: Session 'is_faculty_manager'  value is set to True
-                2.2.2.3: Session 'managed_programs' value is set with the results of the api call
-            2.2.3: If not:
-                2.2.3.1: Context 'is_faculty_manager' value is set to False
-                2.2.3.2: Session 'is_faculty_manager'  value is set to False
-                2.2.3.3: Session 'managed_programs' value is set to None
-    """
-    if request.user.is_authenticated:
-        if request.user.is_superuser or not request.user.has_perm('base.is_student'):
-            if request.session.get('is_faculty_manager') is None:
-                if request.user.has_perm('base.is_faculty_administrator'):
-                    context['is_faculty_manager'] = True
-                    request.session['is_faculty_manager'] = True
-                else:
-                    is_faculty_manager = False
-                    managed_programs = None
-                    managed_programs_as_dict = get_managed_program_as_dict(request.user)
-                    if managed_programs_as_dict:
-                        is_faculty_manager = True
-                        managed_programs = json.dumps(managed_programs_as_dict, cls=DjangoJSONEncoder)
-                    context['is_faculty_manager'] = is_faculty_manager
-                    request.session['is_faculty_manager'] = is_faculty_manager
-                    request.session['managed_programs'] = managed_programs
-            else:
-                context['is_faculty_manager'] = request.session.get('is_faculty_manager')
-        elif not request.user.is_superuser:
-            request.session['is_faculty_manager'] = False
-            request.session['managed_programs'] = None
-            context['is_faculty_manager'] = False
 
 
 def get_managed_program_as_dict(user):
