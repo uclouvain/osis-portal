4fc62b15b6fc106fc31b3b2243f9f914f5572395
Mathieu Zen <zen.mathieu@gmail.com>
2020-02-18 11:41:13 +0100

    add get_object_or_none to models/utils

---
+++ base/models/utils/utils.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+
+def get_object_or_none(klass, *args, **kwargs):
+    try:
+        return get_object_or_404(klass, *args, **kwargs)
+    except Http404:
+        return None
+    except ValueError:
+        klass__name = klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
+        raise ValueError(
+            "First argument to get_object_or_none() must be a Model, Manager, "
+            "or QuerySet, not '%s'." % klass__name
+        )
