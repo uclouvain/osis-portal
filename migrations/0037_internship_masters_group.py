a06248fe79c1eacbddfb1d987621ebc0be39592a
Mathieu Zen <zen.mathieu@gmail.com>
2021-02-10 16:32:26 +0100

    INTERNSHIP-207 clean migration operation

--- migrations/0037_internship_masters_group.py
+++ migrations/0037_internship_masters_group.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.management.sql import emit_post_migrate_signal
 from django.db import migrations
 
 
 def add_init_internship_masters_group(apps, schema_editor):
     # create group
-    db_alias = schema_editor.connection.alias
-    emit_post_migrate_signal(2, False, db_alias)
     Group = apps.get_model('auth', 'Group')
     Permission = apps.get_model('auth', 'Permission')
     internship_masters_group, created = Group.objects.get_or_create(name='internship_masters')
