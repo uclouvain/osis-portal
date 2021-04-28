04aaeba8b5c9907786739ef19d1a4ea2e9ff0371
mathieuzen <zen.mathieu@gmail.com>
2021-04-28 12:06:15 +0200

    INTERNSHIP-254 add user to group internship masters when account already exists

--- views/master_delegates.py
+++ views/master_delegates.py
@@ -25,12 +25,14 @@
 ############################################################################
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
 from django.contrib.messages import SUCCESS
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext as _
 from osis_internship_sdk.models import MasterGet, Person, AllocationGet
 
+from base.models.signals import GROUP_MASTERS_INTERNSHIP
 from base.views import layout
 from internship.decorators.score_encoding_view_decorators import redirect_if_not_master
 from internship.models.enums.role_choice import ChoiceRole
@@ -78,6 +80,7 @@ def new_delegate(request, specialty_uuid, organization_uuid):
             )
             allocation = InternshipAPIService.post_master_allocation(allocation)
             if allocation:
+                _add_existing_user_to_internship_masters_group(person)
                 messages.add_message(
                     request, SUCCESS, _('Internship delegate {} created with success'.format(
                         master.person.last_name
@@ -98,3 +101,17 @@ def delete_delegate(request, allocation_uuid):
 
 def _get_internship_reference(allocation):
     return "{}{}".format(allocation['specialty']['acronym'], allocation['organization']['reference'])
+
+
+def _add_existing_user_to_internship_masters_group(person):
+    existing_user = _get_user_by_email(person.email)
+    if existing_user and not existing_user.groups.filter(name=GROUP_MASTERS_INTERNSHIP).exists():
+        group = Group.objects.get(name=GROUP_MASTERS_INTERNSHIP)
+        existing_user.groups.add(group)
+
+
+def _get_user_by_email(email):
+    try:
+        return User.objects.get(email=email)
+    except User.DoesNotExist:
+        return None
