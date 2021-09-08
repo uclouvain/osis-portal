89c704857933ee999efcc6ea92af0d6961a8189b
Christophe Fivet <christophe.fivet@uclouvain.be>
2021-09-08 11:37:19 +0200

    OSIS-5800 Modification du lien menant aux fonctionnalités d'encodage des notes pour être diriger automatiquement vers OSIS

--- dashboard/views/main.py
+++ dashboard/views/main.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2016 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2021 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -38,6 +38,8 @@ def home(request):
         'manage_courses_url': settings.OSIS_MANAGE_COURSES_URL,
         'osis_vpn_help_url': settings.OSIS_VPN_HELP_URL,
         'dissertation_url': settings.OSIS_DISSERTATION_URL,
+        'score_encoding_url': settings.OSIS_SCORE_ENCODING_URL,
+        'score_encoding_vpn_help_url': settings.OSIS_VPN_HELP_URL
     })
 
 
