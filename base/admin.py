870865d93150c16e5f06c1c10c2c7d53e80c2d86
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-11-05 11:58:19 +0100

    OSIS-6214 : Suppression du model LearningUnitYear et de ses dernières utilisations

--- base/admin.py
+++ base/admin.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2020 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2021 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -51,9 +51,6 @@ admin.site.register(learning_unit.LearningUnit,
 
 admin.site.register(learning_container_year.LearningContainerYear,
                     learning_container_year.LearningContainerYearAdmin)
-
-admin.site.register(learning_unit_year.LearningUnitYear,
-                    learning_unit_year.LearningUnitYearAdmin)
 
 admin.site.register(offer_enrollment.OfferEnrollment,
                     offer_enrollment.OfferEnrollmentAdmin)
