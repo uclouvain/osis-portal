a509dc7cfd484366bcd8f8b89e9a42d70403a5a4
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2022-02-11 08:18:41 +0100

    adapt to code to launch test via intellij

---
+++ frontoffice/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+import sys
+
+import dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if 'test' in sys.argv:
+    os.environ.setdefault('TESTING', 'True')
+dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
+sys.path.extend(os.environ.get('EXTRA_SYS_PATHS').split()) if os.environ.get('EXTRA_SYS_PATHS') else None
+
+SETTINGS_FILE = os.environ.get('DJANGO_SETTINGS_MODULE', 'backoffice.settings.local')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_FILE)
+
