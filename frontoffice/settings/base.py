480dd95171b889d1150afffe6e3acb8b44d55aaf
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2022-04-28 15:40:32 +0200

    OSIS-6698 : Fix hijack template and redirect

--- frontoffice/settings/base.py
+++ frontoffice/settings/base.py
@@ -336,7 +336,6 @@ REST_FRAMEWORK = {
 
 # HIJACK
 HIJACK_LOGIN_REDIRECT_URL = '/'  # Where admins are redirected to after hijacking a user
-HIJACK_LOGOUT_REDIRECT_URL = '/admin/auth/user/'  # Where admins are redirected to after releasing a user
 HIJACK_ALLOW_GET_REQUESTS = True
 HIJACK_USE_BOOTSTRAP = True
 
