c692002d3e1765f51a7d1b5bca3040f72f53f2fa
Christophe Fivet <christophe.fivet@uclouvain.be>
2022-11-30 16:50:39 +0100

    OSIS-7482 Prendre en compte les cas où on est déjà sur une cellule normal

--- attribution/business/xls_students_by_learning_unit.py
+++ attribution/business/xls_students_by_learning_unit.py
@@ -257,7 +257,8 @@ def _set_peps_border(ws, last_row_number):
 def _update_border_for_first_peps_column(cell):
     c = cell.style if cell.has_style else NamedStyle()
     c.border = BORDER_LEFT
-    cell.style = c
+    if not (c.name == 'Normal' and cell.style == 'Normal'):
+        cell.style = c
 
 
 def _add_filters_on_headers(a_worksheet):
