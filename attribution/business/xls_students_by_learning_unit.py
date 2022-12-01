a951bd228850165d8253b68afe75a6957f3508c6
Christophe Fivet <christophe.fivet@uclouvain.be>
2022-12-01 12:40:57 +0100

    Fixed border

--- attribution/business/xls_students_by_learning_unit.py
+++ attribution/business/xls_students_by_learning_unit.py
@@ -255,9 +255,9 @@ def _set_peps_border(ws, last_row_number):
 
 
 def _update_border_for_first_peps_column(cell):
-    c = cell.style if cell.has_style else NamedStyle(name="default")
+    c = cell.style if cell.has_style else NamedStyle()
     c.border = BORDER_LEFT
-    cell.style = c
+    cell.border = c.border
 
 
 def _add_filters_on_headers(a_worksheet):
