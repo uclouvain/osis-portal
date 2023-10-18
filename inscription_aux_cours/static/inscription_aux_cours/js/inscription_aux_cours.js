927df57b9df3323996bfe4eb62e795513cc5d216
Johan Girardin <Johan.Girardin@uclouvain.be>
2023-10-18 11:58:54 +0200

    OSIS 8566 : correction couleur des badges credites et valorisés

--- inscription_aux_cours/static/inscription_aux_cours/js/inscription_aux_cours.js
+++ inscription_aux_cours/static/inscription_aux_cours/js/inscription_aux_cours.js
@@ -13,10 +13,10 @@ function styleRow() {
     document.querySelectorAll('.badge_insc_meme_context').forEach((el)=>el.style.backgroundColor="rgba(3,51,173,0.5)");
     document.querySelectorAll('.badge_insc_different_context').forEach((el)=>el.closest('tr').style.backgroundColor="rgba(46,174,213, 0.07)");
     document.querySelectorAll('.badge_insc_different_context').forEach((el)=>el.style.backgroundColor="rgba(46,174,213,0.87)");
-    document.querySelectorAll('.badge_insc_credite_meme_context, .badge_val_meme_context').forEach((el)=>el.closest('tr').style.backgroundColor="rgba(18, 119, 22, 0.07)");
-    document.querySelectorAll('.badge_insc_credite_meme_context, .badge_val_meme_context').forEach((el)=>el.style.backgroundColor="rgba(18, 119, 22, 0.5)");
-    document.querySelectorAll('.badge_insc_credite_different_context, .badge_val_different_context').forEach((el)=>el.closest('tr').style.backgroundColor="rgba(138, 188, 62, 0.07)");
-    document.querySelectorAll('.badge_insc_credite_different_context, .badge_val_different_context').forEach((el)=>el.style.backgroundColor="rgba(138, 188, 62, 1)");
+    document.querySelectorAll('.insc_credite_meme_context, .val_meme_context').forEach((el)=>el.closest('tr').style.backgroundColor="rgba(0, 102, 68, 0.07)");
+    document.querySelectorAll('.badge_insc_credite_meme_context, .badge_val_meme_context').forEach((el)=>el.style.backgroundColor="rgba(0, 102, 68, 1)");
+    document.querySelectorAll('.insc_credite_different_context, .val_different_context').forEach((el)=>el.closest('tr').style.backgroundColor="rgba(171, 245, 209, 0.2)");
+    document.querySelectorAll('.badge_insc_credite_different_context, .badge_val_different_context').forEach((el)=>{el.style.backgroundColor="rgba(171, 245, 209, 1)";el.style.color="black";});
 }
 
 function initPopover() {
